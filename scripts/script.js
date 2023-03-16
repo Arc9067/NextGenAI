82c9e1879775632dcc8c0886c9b1cb33da1a04bc
Mikail Rokib <atandarokib@gmail.com>
2023-03-16 15:17:17 +0100

    Updated Version

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,16 @@
 "use strict";
 
-let audio = new Audio("../audio.mp3");
-// window.addEventListener("scroll", () => {
-//   audio.play();
-// });
+const loader = document.querySelector(".loader");
+
+gsap.from(".circle", {
+  duration: 1,
+  opacity: 0,
+  y: 150,
+  stagger: 0.25,
+  repeat: -1,
+  yoyo: true,
+});
+
+window.addEventListener("load", () => {
+  loader.style.display = "none";
+});
