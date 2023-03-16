38f0a650d623419adc5b17d887229db4986f067d
Mikail Rokib <atandarokib@gmail.com>
2023-03-16 14:50:55 +0100

    updated

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,6 @@
 "use strict";
 
-/*
-Getting elements
-*/
-const toggleNav = document.querySelector(".toggle-btn");
-const mobileHeader = document.querySelector(".header-nav");
-const closeNav = document.querySelector(".close-nav");
-const moobileLinks = document.querySelectorAll(".mobile-link");
-toggleNav.addEventListener("click", () => {
-  mobileHeader.classList.add("display-header");
-});
-
-closeNav.addEventListener("click", () => {
-  mobileHeader.classList.remove("display-header");
-});
-moobileLinks.forEach((element) => {
-  element.addEventListener("click", () => {
-    mobileHeader.classList.remove("display-header");
-  });
-});
+let audio = new Audio("../audio.mp3");
+// window.addEventListener("scroll", () => {
+//   audio.play();
+// });
